5bbf91211a33f51aa5dc807bf539245d43503cbc
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-11-20 14:13:41 +0200

    Add initial index.py

---
+++ src/index.py
@@ -0,0 +1,11 @@
+from grocerylist import Grocerylist
+from ui.ui import Grocerylist_app
+
+
+def main():
+    application = Grocerylist_app()
+    application.start()  
+
+
+if __name__ == "__main__":
+    main()
