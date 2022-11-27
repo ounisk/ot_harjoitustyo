351ecb8b46ff9bafe8462c08a3ca3c6121c9ac42
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-11-27 16:39:24 +0200

    Add remove and empty_list functionalities

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,34 +1,48 @@
-from grocerylist import Grocerylist
+from services.grocerylist_service import GrocerylistService
+
 
 class Grocerylist_app:
     def __init__(self):
-        self._list = Grocerylist()
+        self._list = GrocerylistService()
 
     def user_instructions(self):
         print("Toiminnot:")
         print("0 - lopeta")
         print("1 - lisää tuote")
-        print("2 - poista tuote")  
-        print("3 - tulosta lista")  
+        print("2 - poista tuote")
+        print("3 - tulosta lista")
+        print("4 - tyhjennä koko lista")
 
     def add(self):
         product = input("Anna tuote: ")
-        self._list.add_product(product)
+        try:
+            product = int(product)
+            print("Annoit numeroita!")
 
-    #def remove(self):
-        #printlist and select product to be removed
-        #product = input
-    
+        except ValueError:
+            self._list.add_product(product)
+
+    def remove(self):  # added 22.11
+        self.print_list()
+        remove_this = input("Anna poistettava tuote: ")
+        self._list.delete_product(remove_this)
+
     def print_list(self):
         list_all = self._list.get_products()
         if list_all:
             for product in list_all:
                 print(product)
-            return    
-        else:
-            print ("Kauppalista on tyhjä.")
             return
-        
+        else:
+            print("Kauppalista on tyhjä.")
+            return
+
+    def empty_list(self):  # added 26.11
+        check = input("Haluatko varmasti tyhjentää koko listan - kyllä/ei?: ")
+        if check == "kyllä":
+            self._list.empty_whole_list()
+        else:
+            return self.user_instructions()
 
     def start(self):
         self.user_instructions()
@@ -38,9 +52,11 @@ class Grocerylist_app:
                 break
             elif action == "1":
                 self.add()
-            #elif action == "2":  #add this
-            #    self.remove()        
+            elif action == "2":  # added 22.11
+                self.remove()
             elif action == "3":
-                self.print_list()       
+                self.print_list()
+            elif action == "4":
+                self.empty_list()
             else:
                 self.user_instructions()
