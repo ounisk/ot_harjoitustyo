d881ce458889a6b7d97af831bbf6badc83a9b9e4
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-11-28 09:21:42 +0200

    Update file with dotenv

--- src/config.py
+++ src/config.py
@@ -1,15 +1,15 @@
 import os
-#from dotenv import load_dotenv  #tästä tulee error vaikka on lisätty poetryyn
+from dotenv import load_dotenv #error
 
 dirname = os.path.dirname(__file__)
 
-data_file_path = os.path.join(dirname, "data.csv")  # toimii kun tää akt
+#data_file_path = os.path.join(dirname, "data.csv")  # toimii kun tää akt
 
-#try:
-#    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))    #ei toimi kun tää akt
+try:
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))    #ei toimi kun tää akt
     #load_dotenv(dotenv_path=os.path.join(dirname, "data.csv"))       #ei toimi kun tää akt
-#except FileNotFoundError:                                            #ei toimi kun tää akt
-#    pass                                                             #ei toimi kun tää akt
+except FileNotFoundError:                                            #ei toimi kun tää akt
+    pass                                                             #ei toimi kun tää akt
 
 ALIST_FILENAME = os.getenv("ALIST_FILENAME") or "grocerylist.csv"
 ALIST_FILE_PATH = os.path.join(dirname, "..", "data", ALIST_FILENAME)
