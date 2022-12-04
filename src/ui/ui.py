d5d9f77c92c265b0186d7fc0e789d46185e3404e
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-12-04 14:04:17 +0200

    Add quantity&store, improve code

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,4 +1,4 @@
-from services.grocerylist_service import GrocerylistService
+from services.grocerylist_service import GrocerylistService, ProductAlreadyOnListError, ProductNotOnListError
 
 
 class Grocerylist_app:
@@ -14,27 +14,66 @@ class Grocerylist_app:
         print("4 - tyhjennä koko lista")
 
     def add(self):
+        store = input("Lisää tuote kauppaan - K-market vai Lidl?: ")
+        if store not in ["K-market", "Lidl"]:  # lisäys 2.12
+            print("Kauppa ei valittavissa!")
+            return
         product = input("Anna tuote: ")
-        #quantity = input ("Anna määrä: ")    #added . 28.11
+        if not (product.strip()):
+            print("Annoit tyhjän tuotekentän!")
+            return
+        quantity = input("Anna määrä, jos haluat: ")
         try:
             product = int(product)
             print("Annoit numeroita!")
 
         except ValueError:
-            self._list.add_product(product) #, quantity) #muok. 28.11
+            try:
+                self._list.add_product(product, quantity, store)
+            except ProductAlreadyOnListError:  # 4.12
+                print("Tuote on jo listalla!")
 
     def remove(self):  # added 22.11
         self.print_list()
+        remove_from = input("Poista kaupan listalta - K-market vai Lidl?: ")
         remove_this = input("Anna poistettava tuote: ")
-        self._list.delete_product(remove_this)
+
+        try:
+            remove_this = int(remove_this)
+            print("Annoit numeroita!")
+
+        except ValueError:
+            try:
+                self._list.delete_product(remove_this, remove_from)
+            except ProductNotOnListError:
+                print("Tuotetta ei ole listalla!")
 
     def print_list(self):
         list_all = self._list.get_products()
+
         if list_all:
-            for product in list_all:
-                print(product)
-            return
+            print("K-MARKET:")
+            list_store1 = self._list.get_products_store("K-market")
+            if list_store1:
+                for product in list_store1:
+                    print(product[0], product[1])
+                    #print (f"{product[0]:10}  {product[1]:10}")
+            else:
+                print("Kauppalista on tyhjä.")
+
+            print("LIDL:")
+            list_store2 = self._list.get_products_store("Lidl")
+            if list_store2:
+                for product in list_store2:
+                    print(product[0], product[1])
+                    #print (f"{product[0]:10}  {product[1]:10}")
+            else:
+                print("Kauppalista on tyhjä.")
+
         else:
+            print("K-MARKET:")
+            print("Kauppalista on tyhjä.")
+            print("LIDL:")
             print("Kauppalista on tyhjä.")
             return
 
@@ -46,6 +85,7 @@ class Grocerylist_app:
             return self.user_instructions()
 
     def start(self):
+        print("***OSTOSLISTA***")
         self.user_instructions()
         while True:
             action = input("Valitse toiminto: ")
@@ -53,7 +93,7 @@ class Grocerylist_app:
                 break
             elif action == "1":
                 self.add()
-            elif action == "2":  # added 22.11
+            elif action == "2":
                 self.remove()
             elif action == "3":
                 self.print_list()
