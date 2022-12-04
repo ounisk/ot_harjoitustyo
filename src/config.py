29111720b36f05b381371a9715d9bbe63a919084
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-12-04 13:52:33 +0200

    Update database configuration

--- src/config.py
+++ src/config.py
@@ -1,16 +1,19 @@
 import os
-from dotenv import load_dotenv #error
+from dotenv import load_dotenv  # joskus error
 
 dirname = os.path.dirname(__file__)
 
-#data_file_path = os.path.join(dirname, "data.csv")  # toimii kun tää akt
+# data_file_path = os.path.join(dirname, "data_base.sqlite")  #VSC:ssa toimiva versio
 
 try:
-    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))    #ei toimi kun tää akt
-    #load_dotenv(dotenv_path=os.path.join(dirname, "data.csv"))       #ei toimi kun tää akt
-except FileNotFoundError:                                            #ei toimi kun tää akt
-    pass                                                             #ei toimi kun tää akt
+    # termin toimii VSC:ssa
+    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
 
-ALIST_FILENAME = os.getenv("ALIST_FILENAME") or "grocerylist.csv"
-ALIST_FILE_PATH = os.path.join(dirname, "..", "data", ALIST_FILENAME)
-# ALIST_FILE_PATH = os.path.join(dirname, "..", "data" ALIST_FILENAME)  
+except FileNotFoundError:  # ei toimi kun tää akt
+    pass  # ei toimi kun tää akt
+
+DATABASE_FILENAME = os.getenv(
+    "DATABASE_FILENAME") or "database_grocerylist.sqlite"
+DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
+#ALIST_FILENAME = os.getenv("ALIST_FILENAME") or "grocerylist.csv"
+#ALIST_FILE_PATH = os.path.join(dirname, "..", "data", ALIST_FILENAME)
