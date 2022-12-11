56f0b7945641a6e078dd6c6a5a8d6c29ecf910c4
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-12-11 16:49:35 +0200

    Remove unused code

--- src/config.py
+++ src/config.py
@@ -1,19 +1,13 @@
 import os
-from dotenv import load_dotenv  # joskus error
+from dotenv import load_dotenv
 
 dirname = os.path.dirname(__file__)
 
-# data_file_path = os.path.join(dirname, "data_base.sqlite")  #VSC:ssa toimiva versio
-
 try:
-    # termin toimii VSC:ssa
     load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
-
-except FileNotFoundError:  # ei toimi kun tää akt
-    pass  # ei toimi kun tää akt
+except FileNotFoundError:
+    pass
 
 DATABASE_FILENAME = os.getenv(
     "DATABASE_FILENAME") or "database_grocerylist.sqlite"
 DATABASE_FILE_PATH = os.path.join(dirname, "..", "data", DATABASE_FILENAME)
-#ALIST_FILENAME = os.getenv("ALIST_FILENAME") or "grocerylist.csv"
-#ALIST_FILE_PATH = os.path.join(dirname, "..", "data", ALIST_FILENAME)
