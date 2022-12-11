a6106d909545c3f6a451d183e7fb3809a5b8217c
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-12-11 17:41:52 +0200

    Add termcolor, 2 new funtions

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,4 +1,5 @@
 from services.grocerylist_service import GrocerylistService, ProductAlreadyOnListError, ProductNotOnListError
+from termcolor import cprint
 
 
 class Grocerylist_app:
@@ -10,12 +11,14 @@ class Grocerylist_app:
         print("0 - lopeta")
         print("1 - lisää tuote")
         print("2 - poista tuote")
-        print("3 - tulosta lista")
-        print("4 - tyhjennä koko lista")
+        print("3 - muokkaa tuotteen määrää")
+        print("4 - tulosta lista")
+        print("5 - näytä tuotteiden top3")
+        print("6 - tyhjennä koko lista")
 
     def add(self):
         store = input("Lisää tuote kauppaan - K-market vai Lidl?: ")
-        if store not in ["K-market", "Lidl"]:  # lisäys 2.12
+        if store not in ["K-market", "Lidl"]:
             print("Kauppa ei valittavissa!")
             return
         product = input("Anna tuote: ")
@@ -30,10 +33,10 @@ class Grocerylist_app:
         except ValueError:
             try:
                 self._list.add_product(product, quantity, store)
-            except ProductAlreadyOnListError:  # 4.12
+            except ProductAlreadyOnListError:
                 print("Tuote on jo listalla!")
 
-    def remove(self):  # added 22.11
+    def remove(self):
         self.print_list()
         remove_from = input("Poista kaupan listalta - K-market vai Lidl?: ")
         remove_this = input("Anna poistettava tuote: ")
@@ -48,44 +51,69 @@ class Grocerylist_app:
             except ProductNotOnListError:
                 print("Tuotetta ei ole listalla!")
 
+    def modify_product_quantity(self):
+        self.print_list()
+        modify_from = input("Muokkaa määrää listalla - K-market vai Lidl?: ")
+        if modify_from not in ["K-market", "Lidl"]:
+            print("Kauppaa ei ole!")
+            return
+        modify_product = input("Anna tuote, jonka määrää haluat muokata: ")
+        if not (modify_product.strip()):
+            print("Annoit tyhjän tuotekentän!")
+            return
+
+        new_quantity = input("Anna uusi määrä: ")
+        try:
+            self._list.modify_quantity(
+                modify_product, new_quantity, modify_from)
+        except ProductNotOnListError:
+            print("Tuotetta ei ole listalla!")
+
     def print_list(self):
         list_all = self._list.get_products()
 
         if list_all:
-            print("K-MARKET:")
+            cprint("K-MARKET:", "green")
             list_store1 = self._list.get_products_store("K-market")
             if list_store1:
                 for product in list_store1:
                     print(product[0], product[1])
-                    #print (f"{product[0]:10}  {product[1]:10}")
             else:
                 print("Kauppalista on tyhjä.")
 
-            print("LIDL:")
+            cprint("LIDL:", "green")
             list_store2 = self._list.get_products_store("Lidl")
             if list_store2:
                 for product in list_store2:
                     print(product[0], product[1])
-                    #print (f"{product[0]:10}  {product[1]:10}")
             else:
                 print("Kauppalista on tyhjä.")
 
         else:
-            print("K-MARKET:")
+            cprint("K-MARKET:", "green")
             print("Kauppalista on tyhjä.")
-            print("LIDL:")
+            cprint("LIDL:", "green")
             print("Kauppalista on tyhjä.")
             return
 
-    def empty_list(self):  # added 26.11
+    def empty_list(self):
         check = input("Haluatko varmasti tyhjentää koko listan - kylla/ei?: ")
         if check == "kylla":
             self._list.empty_whole_list()
         else:
             return self.user_instructions()
 
+    def get_top3(self):
+        list_top3 = self._list.get_top3_products()
+        if list_top3:
+            cprint("TOP3 tuotteet:", "green")
+            for product in list_top3:
+                print(product[0])
+        else:
+            print("TOP3 on tyhjä.")
+
     def start(self):
-        print("***OSTOSLISTA***")
+        cprint("***OSTOSLISTA***", "yellow", attrs=["bold"])
         self.user_instructions()
         while True:
             action = input("Valitse toiminto: ")
@@ -96,8 +124,12 @@ class Grocerylist_app:
             elif action == "2":
                 self.remove()
             elif action == "3":
-                self.print_list()
+                self.modify_product_quantity()
             elif action == "4":
+                self.print_list()
+            elif action == "5":
+                self.get_top3()
+            elif action == "6":
                 self.empty_list()
             else:
                 self.user_instructions()
