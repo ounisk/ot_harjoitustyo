cc91c99c3912d894de2070a4a0621e2e3dbb3be7
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2022-12-11 16:58:04 +0200

    Add docstring comments

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -2,16 +2,29 @@ from database_connection import get_database_connection
 
 
 def drop_tables(connection):
+    """Poistaa tietokantataulut ja kaiken sisällön.
+
+    Args:
+        connection (connection -olio): olio tietokantayhteyttä varten.
+    """
     cursor = connection.cursor()
     cursor.execute('''
         DROP TABLE IF EXISTS GROCERIES 
         ''')
+
+    cursor.execute('''
+        DROP TABLE IF EXISTS HISTORY 
+        ''')
+
     connection.commit()
 
 
 def create_tables(connection):
+    """Luo tietokantataulut.
+    Args:
+        connection (connection -olio): olio tietokantayhteyttä varten.
+    """
     cursor = connection.cursor()
-
     cursor.execute('''
         CREATE TABLE GROCERIES (
             id INTEGER PRIMARY KEY,
@@ -20,10 +33,23 @@ def create_tables(connection):
             store TEXT
         );
     ''')
+    cursor.execute('''
+        CREATE TABLE HISTORY (
+            id INTEGER PRIMARY KEY,
+            product TEXT,
+            store TEXT
+        );
+    ''')
+
     connection.commit()
 
 
 def initialize_database():
+    """Metodikutsu, joka aktivoi tietokantayhteyden mahdollistavan yhteyden.
+
+    Lisäksi alustaa taulut: kutsuu tietokantataulujen poisto- ja luonti-metodit.
+
+    """
 
     connection = get_database_connection()
 
